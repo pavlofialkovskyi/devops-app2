ced76d6209947b226be27c061d3b68b77275a835
Pavlo <pavelfialkovsky@gmail.com>
2026-09-22 15:39:49 -0400

    alb_sg => ec2_sg => rds_sg, as I recreated a new vpc with new ID

---
+++ terraform/security_groups.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allows inbound requests from www via http and https"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allows app traffic from the ALB only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "App port from ALB"
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allows Postgres traffic from EC2 only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Postgres from EC2"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
