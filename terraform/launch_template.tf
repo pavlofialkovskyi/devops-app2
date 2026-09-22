c6a8a2d5b99ba487ee35431102c074f381a3ced2
Pavlo <pavelfialkovsky@gmail.com>
2026-09-22 15:56:31 -0400

    using my console asg template setup but now in IaC terraform

---
+++ terraform/launch_template.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_launch_template" "app" {
+  name_prefix   = "devops-app2-"
+  image_id      = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.micro"
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = filebase64("${path.module}/../scripts/bootstrap.sh")
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "devops-app2"
+    }
+  }
+}
