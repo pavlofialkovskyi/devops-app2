f93f91119f0f40654f04f194b6064e36d3d1755a
Pavlo <pavelfialkovsky@gmail.com>
2026-09-23 02:42:40 -0400

    changed the subnets for EC2 from Public to Private

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -3,7 +3,7 @@ resource "aws_autoscaling_group" "app" {
   min_size            = 1
   desired_capacity    = 1
   max_size            = 3
-  vpc_zone_identifier = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  vpc_zone_identifier = [aws_subnet.private_app_a.id, aws_subnet.private_app_b.id]
 
   health_check_type = "ELB"
   target_group_arns = [aws_lb_target_group.app.arn]
